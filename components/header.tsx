f07759b9f9b2483f8fd346bca792fda448cb8364
Alex Kostyniuk <kostyniuk7@gmail.com>
2025-12-30 17:23:56 +0100

    first commit with a landing page

--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,25 @@
 import Image from "next/image";
 import { ThemeToggle } from "./theme-toggle";
+import Link from "next/link";
+
 
 export async function Header() {
 
   return (
-    <header className="flex-none h-12 border-b flex items-center justify-between px-4 bg-background z-20">
-      <div className="flex items-center gap-2">
-        <Image
-          src="/favicon.svg"
-          alt="Mellow Lines"
-          width={24}
-          height={24}
-        />
-        <h1 className="text-lg font-bold bg-gradient-to-r from-foreground to-muted-foreground bg-clip-text text-transparent">
-          Mellow Lines
-        </h1>
-      </div>
+    <header className="flex-none h-14 border-b flex items-center justify-between px-4 bg-background z-20">
+      <Link href="/">
+        <div className="flex items-center gap-2">
+          <Image
+            src="/favicon.svg"
+            alt="Mellow Lines"
+            width={24}
+            height={24}
+          />
+          <h1 className="text-lg font-bold bg-gradient-to-r from-foreground to-muted-foreground bg-clip-text text-transparent">
+            Mellow Lines
+          </h1>
+        </div>
+      </Link>
       <ThemeToggle />
     </header>
   );
